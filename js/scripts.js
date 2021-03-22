5a6aa2de5d2609ff15bc1f9bc620de11801993d1
basicjohn <edmondsonj@gmail.com>
2021-03-22 16:26:27 -0700

    created basic file structure

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a Number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));
