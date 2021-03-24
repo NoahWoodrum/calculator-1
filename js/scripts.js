00a454ecf13f527fbd8653cee57ab64ea8cd1076
basicjohn <edmondsonj@gmail.com>
2021-03-24 14:57:25 -0700

    WIP:created and formated calculator
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>
    Co-authored-by: Noah Woodrum <heymymomthinksimcool@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
+// Business logic:
+
 function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a Number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
-alert(add(number1, number2));
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// Everything below this line is user interface logic:
+
+$(document).ready(function () {
+  $("form#add").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
