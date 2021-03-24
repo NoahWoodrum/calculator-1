679f94ffe0e1de20e23a11f9a5f74d571f686bcc
basicjohn <edmondsonj@gmail.com>
2021-03-24 15:17:46 -0700

    created the scripts for each button to calculate
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>
    Co-authored-by: Noah Woodrum <heymymomthinksimcool@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,44 @@
 // Business logic:
-
 function add(number1, number2) {
   return number1 + number2;
 }
-
 function subtract(number1, number2) {
   return number1 - number2;
 }
-
 function multiply(number1, number2) {
   return number1 * number2;
 }
-
 function divide(number1, number2) {
   return number1 / number2;
 }
-
 // Everything below this line is user interface logic:
-
 $(document).ready(function () {
-  $("form#add").submit(function (event) {
+    $(".add").click(function (event) {
     event.preventDefault();
     const number1 = parseInt($("#num1").val());
     const number2 = parseInt($("#num2").val());
     const result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $(".subtract").click(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $(".multiply").click(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    const result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $(".divide").click(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    const result = divide(number1, number2);
     $("#output").text(result);
   });
 });
